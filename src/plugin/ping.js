09617e9a4354971f4604ad954cdfd2c142ff9a31
marisela2 <shenapam3@gmail.com>
2024-09-28 01:15:06 +0300

    Mercedes

--- src/plugin/ping.js
+++ src/plugin/ping.js
@@ -11,7 +11,7 @@ const text = m.body.slice(prefix.length + cmd.length).trim();
     const end = new Date().getTime();
     const responseTime = (end - start) / 1000;
 
-    const text = `*_🔥⃝вσт ѕρєє∂: ${responseTime.toFixed(2)} s_*`;
+    const text = `*_Pong: ${responseTime.toFixed(2)} s_*`;
     sock.sendMessage(m.from, { text }, { quoted: m });
   }
 }
