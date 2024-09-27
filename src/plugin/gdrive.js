e7b376dd25d36b712aef47a1c0bce7f2c1c45616
marisela2 <shenapam3@gmail.com>
2024-09-28 02:58:19 +0300

    Mercedes

--- src/plugin/gdrive.js
+++ src/plugin/gdrive.js
@@ -20,7 +20,7 @@ const text = m.body.slice(prefix.length + cmd.length).trim();
 
       if (gdriveInfo && gdriveInfo.status && gdriveInfo.data) {
         const mediaUrl = gdriveInfo.data;
-        const caption = `> © Powered By Ethix-MD`;
+        const caption = `> © Powered By Mercedes`;
 
         // Inferring the file type based on the file extension
         const extension = mediaUrl.split('.').pop().toLowerCase();
