70c8be0bf9dcf0fb74a78ea265a2764011586e71
marisela2 <shenapam3@gmail.com>
2024-09-28 00:58:20 +0300

    Mercedes

--- src/plugin/sticker.js
+++ src/plugin/sticker.js
@@ -6,7 +6,7 @@ const stickerCommand = async (m, gss) => {
   const cmd = m.body.startsWith(prefix) ? m.body.slice(prefix.length).split(' ')[0].toLowerCase() : '';
   const text = m.body.slice(prefix.length + cmd.length).trim();
 
-  const packname = global.packname || "𝐄𝐭𝐡𝐢𝐱-𝐌𝐃";
+  const packname = global.packname || "Mercedes";
   const author = global.author || "🥵💫👿";
 
   const validCommands = ['sticker', 's', 'autosticker'];
