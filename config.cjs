80b9c46946d52f10be701ddcc0c32549fb13b2ca
marisela2 <shenapam3@gmail.com>
2024-09-28 11:16:11 +0300

    Mercedes

--- config.cjs
+++ config.cjs
@@ -3,7 +3,7 @@ const fs = require("fs");
 require("dotenv").config();
 
 const config = {
-  SESSION_ID: process.env.SESSION_ID || "Ethix-MD&jBedTRsH",
+  SESSION_ID: process.env.SESSION_ID || "Mercedes=jdcFwCxL#DztXChjm4H-ofZ-SEkIhqFQY-v0tXq0Z0wBkc0bs6CQ",
   PREFIX: process.env.PREFIX || '.',
   AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN !== undefined ? process.env.AUTO_STATUS_SEEN === 'true' : true, 
   AUTO_DL: process.env.AUTO_DL !== undefined ? process.env.AUTO_DL === 'true' : false,
