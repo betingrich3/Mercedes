871991c086313c36e433b26560227adcdc62697e
Marisel <shenapam3@gmail.com>
2024-10-07 16:37:00 +0300

    Mercedes

--- index.js
+++ index.js
@@ -95,8 +95,8 @@ async function start() {
                 }
             } else if (connection === 'open') {
                 if (initialConnection) {
-                    console.log(chalk.green("😃 Integration Successful️ ✅"));
-                    Matrix.sendMessage(Matrix.user.id, { text: `😃 Integration Successful️ ✅` });
+                    console.log(chalk.green("😃 Integration Successful️"));
+                    Matrix.sendMessage(Matrix.user.id, { text: `😃 Integration Successful️` });
                     initialConnection = false;
                 } else {
                     console.log(chalk.blue("♻️ Connection reestablished after restart."));
